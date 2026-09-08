a630f1ab42f98211667f4269c83110df9fbeaadb
SenItaNa <hurongjie@qianban.online>
2026-09-08 22:18:46 +0800

    发布渝安青澜 v2.1.1 (#29)
    * chore(release): bump version to 2.1.1
    
    * docs: archive v2.1.1 changelog
    
    * docs: add v2.1.1 release notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.yuanqinglan.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "2.1.0"
+        versionCode = 6
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
